634287b4a23336715823b9037793b30735ae9168
Thibaud Arnault <thibaud.arnault@gmail.com>
2018-03-02 09:47:36 -0400

    Revert commit on IDatabaseConfig - found better workaround

--- src/database.model.ts
+++ src/database.model.ts
@@ -1,13 +1,22 @@
-export interface IDatabaseConfig {
+export interface ISQLiteDatabase {
+	type: string,
+	storage: string,
+	live?: IDatabaseConfig
+}
+
+export interface ISQLDatabase {
 	type: string,
 	host: string,
 	port: number,
 	database: string,
 	username: string,
 	password?: string,
-	storage?: string, // SQLite
 	live?: IDatabaseConfig
 }
+
+export type IDatabaseConfig
+	= ISQLiteDatabase
+	| ISQLDatabase;
 
 export interface IDatabase {
 	dev?: IDatabaseConfig,
