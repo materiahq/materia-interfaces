b7cb0c00324689f33247791e2aadd680aae82f88
GeoAstronaute <jeanmarc.royy@gmail.com>
2019-02-20 18:40:59 +0100

    (Addon) Add lastVersion to IAddon

--- src/addon.model.ts
+++ src/addon.model.ts
@@ -1,31 +1,26 @@
 export interface IAddon {
 	package: string
 	name: string
-	path?: string
-	config?: any
-	setupConfig?: any[]
-	banner?: string
-
 	description: string
 	logo: string
 	author: string
 	version: string
-
-	tags?: Array<{ id: string }>,
-	category?: string,
-
-	color?: string,
-
-	enabled: boolean,
+	enabled: boolean
 	installed: boolean
 	installing: boolean
 
-	git?: string,
-
-	readme?: string,
-	changelog?: string,
-
-	published: any
+	published?: boolean
+	lastVersion?: string
+	path?: string
+	config?: any
+	setupConfig?: any[]
+	banner?: string
+	tags?: Array<{ id: string }>
+	category?: string
+	git?: string
+	readme?: string
+	changelog?: string
+	color?: string
 }
 
 export interface IAddonTag {
